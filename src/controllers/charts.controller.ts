59a5667ab14054a91f4e0c32e9df9031033c447f
Stefanos Hadjipetrou <stefanos@zimmerman.team>
2023-05-11 18:48:02 +0300

    feat: add chart sample data api function

--- src/controllers/charts.controller.ts
+++ src/controllers/charts.controller.ts
@@ -18,6 +18,7 @@ import {
   response,
 } from '@loopback/rest';
 import axios from 'axios';
+import _ from 'lodash';
 import {Chart} from '../models';
 import {ChartRepository} from '../repositories';
 
@@ -46,6 +47,92 @@ export class ChartsController {
     chart: Omit<Chart, 'id'>,
   ): Promise<Chart> {
     return this.chartRepository.create(chart);
+  }
+
+  @get('/chart/sample-data/{datasetId}')
+  @response(200)
+  async sampleData(@param.path.string('datasetId') datasetId: string) {
+    return axios
+      .get(`http://localhost:4400/sample-data/${datasetId}/?returnAll=1`)
+      .then(res => {
+        const rows = _.get(res, 'data.data', []);
+        const columns = Object.keys(_.get(rows, '[0]', {}));
+
+        const stats: {
+          name: string;
+          type: string;
+          data: {name: string; value: number}[];
+        }[] = [];
+
+        columns.forEach((c: string) => {
+          const grouped = _.groupBy(rows, c);
+          const keys = Object.keys(grouped);
+          if (
+            keys.length < 4 ||
+            (keys.length < rows.length / 1.5 && keys.length > 20)
+          ) {
+            const total = rows.length;
+            stats.push({
+              name: c,
+              type: 'percentage',
+              data: _.orderBy(
+                keys.map(k => ({
+                  name: k,
+                  value: (grouped[k].length / total) * 100,
+                })),
+                'value',
+                'desc',
+              ),
+            });
+            if (stats[stats.length - 1].data.length > 20) {
+              const others = stats[stats.length - 1].data.slice(2);
+              stats[stats.length - 1].data = stats[stats.length - 1].data.slice(
+                0,
+                2,
+              );
+              stats[stats.length - 1].data.push({
+                name: 'Others',
+                value: _.sumBy(others, 'value'),
+              });
+            }
+          } else if (keys.length < 21) {
+            stats.push({
+              name: c,
+              type: 'bar',
+              data: _.sortBy(keys).map(k => ({
+                name: k,
+                value: grouped[k].length,
+              })),
+            });
+          } else {
+            stats.push({
+              name: c,
+              type: 'unique',
+              data: [
+                {
+                  name: 'Unique',
+                  value: keys.length,
+                },
+              ],
+            });
+          }
+        });
+
+        return {
+          count: _.get(res, 'data.count', []),
+          sample: _.get(res, 'data.sample', []),
+          dataTypes: _.get(res, 'data.dataTypes', []),
+          filterOptionGroups: _.get(res, 'data.filterOptionGroups', []),
+          stats,
+        };
+      })
+      .catch(error => {
+        console.log(error);
+        return {
+          data: [],
+          error,
+        };
+      });
   }
 
   @get('/charts/count')
