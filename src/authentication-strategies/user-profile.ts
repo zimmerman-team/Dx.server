9814b72fb66d0397b0f69e8683d77edddf503922
Stefanos Hadjipetrou <stefanos@zimmerman.team>
2024-12-19 12:35:30 +0200

    feat: payment flow changes

--- src/authentication-strategies/user-profile.ts
+++ src/authentication-strategies/user-profile.ts
@@ -1,4 +1,11 @@
+import _ from 'lodash';
+import Stripe from 'stripe';
 import {AUTH0_MGMT_API_CALL} from '../utils/auth';
+
+const StripeClient = new Stripe(process.env.STRIPE_API_KEY as string, {
+  // @ts-ignore
+  apiVersion: null,
+});
 
 export class UserProfile {
   static async getUserProfile(userId: string): Promise<any> {
@@ -7,6 +14,11 @@ export class UserProfile {
   }
   //delete user
   static async deleteUser(userId: string): Promise<any> {
+    const userProfile = await this.getUserProfile(userId);
+    const customerId = _.get(userProfile, 'app_metadata.stripeCustomerId');
+    if (customerId) {
+      await StripeClient.customers.del(customerId);
+    }
     const data = await AUTH0_MGMT_API_CALL('DELETE', `users/${userId}`);
     return data;
   }
